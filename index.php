c8add7f9b89648781296bfff83abef53a63671c2
YangSW1126 <yangshaowei911126@gmail.com>
2023-12-07 09:48:05 +0800

    d

--- index.php
+++ index.php
@@ -4,7 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "stylesheet" href = "css/style_index.css?v=<?=time()?>">
-    <script async src = "https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script async src = "https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src = "js/script.js"></script>
+    
+    
+    <title>Document</title>
+    
+</head>
+<body>
+    <?php include "header.html" ?>
+    <div class = "container">
+    sad test123
+    </div>
+    <p>css test</p>
+    <div id="includeFooter"></div>
     <script>
         $(function() {
             $("#includeHeader").load("header.html");
@@ -13,18 +26,7 @@
         $(function() {
             $("#includeFooter").load("footer.html");
         });
-    </script>
-    
-    <title>Document</title>
-    
-</head>
-<body>
-    <div id="includeHeader"></div>
-    <div class = "container">
-    sad test123
-    </div>
-    <p>css test</p>
-    <div id="includeFooter"></div>
+        </script>
 </body>
 </html>
 
