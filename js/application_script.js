b8feeb16a9652fd14ebad6b9f7e3cb1c0c8bbad6
YangSW1126 <113578427+YangSW1126@users.noreply.github.com>
2023-12-11 16:59:48 +0800

    add

---
+++ js/application_script.js
@@ -0,0 +1,29 @@
+var conditions = ["年滿65歲", "設籍並實際居住本市", "123", "456"];
+var query = [1,1,0,0];
+
+$(document).ready(function(){
+}); 
+
+function displayCondition(){
+    for(i = 0; i < 4; i++){
+        if(query[i] == 1){
+            const para = document.createElement("li");
+            const node = document.createTextNode(conditions[i]);
+            para.appendChild(node);
+            const element = document.getElementById("condition-ul");
+            element.appendChild(para);
+        }
+    }
+
+}
+
+
+function removeCondition(condition){
+    const element = document.getElementById(condition);
+    element.remove();
+    query[condition] = 0;
+}
+
+function editCondition(){
+    $('#addConditionModal').modal('hide');
+}
