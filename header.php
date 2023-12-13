10babc5f6a97bfb813002f88eb8d9fb25a661be0
YangSW1126 <yangshaowei911126@gmail.com>
2023-12-13 17:15:19 +0800

    add

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "css/style_header.css?v=<?=time()?>">
+    
+    <title>Document</title>
+</head>
+<body>
+    <div class = "header-bg">
+        <div><a href = "https://www.facebook.com/profile.php?id=100090699375511" target="_blank" class = "icon-link"><img class = "icon" src = "img/icon.png"><a></div>
+        <div class = "logo-name"><a href = "https://www.facebook.com/profile.php?id=100090699375511" target="_blank" class = "icon-link">汪汪汪</a></div>
+        <div class = "header-row">
+            <ul>
+                <li class = "link"><a href = "https://www.facebook.com/profile.php?id=100090699375511" target="_blank">項目1</a></li>
+                <li class = "link"><a href = "https://www.facebook.com/profile.php?id=100090699375511" target="_blank">項目2</a></li>
+                <li class = "link"><a href = "https://www.facebook.com/profile.php?id=100090699375511" target="_blank">項目3</a></li>
+                <li class = "link"><a href = "https://www.facebook.com/profile.php?id=100090699375511" target="_blank">項目4</a></li>
+                <li class = "link"><a href = "https://www.facebook.com/profile.php?id=100090699375511" target="_blank">項目5</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "css/style_footer.css?v=<?=time()?>">
+    <link rel = "stylesheet" href = "css/style_header.css?v=<?=time()?>">
+    <link rel = "stylesheet" href = "css/style_login.css?v=<?=time()?>">
+    
+    <title>Document</title>
+    
+</head>
+<body>
+    <?php
+        include "header.php";
+    ?>
+
+    <div class = "container">
+    sad test12356
+    </div>
+    <p>css test</p>
+
+    <?php
+        include "footer.php";
+    ?>
+
+</body>
+</html>
